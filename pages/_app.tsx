de1335370a38ab496f830c8c2df8ee6810ebca8c
Robert van den Boorn <robertb@uniform.dev>
2024-07-22 15:15:54 +0200

    feat: added edge output type for Vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
-import '../styles/style.css';
+import "../styles/style.css";
 
-import { UniformAppProps } from '@uniformdev/context-next';
-import { UniformContext } from '@uniformdev/context-react';
+import { UniformAppProps } from "@uniformdev/context-next";
+import { UniformContext } from "@uniformdev/context-react";
 
-import { createUniformContext } from '../lib/context/uniformContext';
+import { createUniformContext } from "../lib/context/uniformContext";
 
 const clientContext = createUniformContext();
 
-export default function UniformConfApp({ Component, pageProps, serverUniformContext }: UniformAppProps) {
+export default function UniformConfApp({
+  Component,
+  pageProps,
+  serverUniformContext,
+}: UniformAppProps) {
+  const outputType = process.env.VERCEL_URL ? "edge" : "standard";
+
   return (
-    <UniformContext context={serverUniformContext ?? clientContext}>
+    <UniformContext
+      context={serverUniformContext ?? clientContext}
+      outputType={outputType}
+    >
       <Component {...pageProps} />
     </UniformContext>
   );
