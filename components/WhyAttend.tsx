ee23230de3a5e756273bb2561254778af0f53571
Robert van den Boorn <robertb@uniform.dev>
2024-07-23 12:28:23 +0200

    feat: updated styling of the why attend component

--- components/WhyAttend.tsx
+++ components/WhyAttend.tsx
@@ -11,33 +11,54 @@ export type WhyAttendProps = ComponentProps<{
   image: string;
 }>;
 
-export const WhyAttend = ({ title, text, image, component }: WhyAttendProps) => (
-  <section className="bg-white border-b py-8">
-    <div
-      className="container mx-auto flex flex-wrap pt-4 pb-12"
-      style={{
-        flexDirection: component.variant === 'whyattendleft' ? 'row' : 'row-reverse',
-      }}
-    >
-      {image ? (
-        <div className="w-1/2">
-          <img src={image} alt={title} width={400} height={400} loading="lazy" className="p-10" />
+export const WhyAttend = ({ title, text, image, component }: WhyAttendProps) => {
+  const variant = component.variant === 'whyattendleft' ? 'Left' : 'Right';
+  const labelStyle = variant === 'Left' ? 'bg-green-100 text-green-800' : 'bg-red-800 text-white';
+
+  return (
+    <section className="bg-white border-b py-8">
+      <div className="container max-w-5xl mx-auto m-8">
+        <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+          {title}
+        </h1>
+        <div className="w-full mb-4">
+          <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t" />
         </div>
-      ) : null}
-      <div className="w-1/2">
-        <div className="p-10">
-          <h2
-            className="w-full my-2 text-4xl font-bold leading-tight text-center text-gray-800"
-            dangerouslySetInnerHTML={{ __html: title }}
-          />
-          <hr />
-          <p className="text-gray-800 p-10 whitespace-pre-line" dangerouslySetInnerHTML={{ __html: text }} />
+        <div
+          className={`flex flex-wrap ${variant === 'Left' ? 'flex-row-reverse' : ''}`}
+        >
+          <div className="w-full sm:w-1/2 p-6 mt-6">
+            <ImageComponent image={image} />
+          </div>
+          <div className="w-full sm:w-1/2 p-6 mt-6 flex flex-col justify-center">
+            <div className="flex items-center justify-center sm:justify-start mb-4">
+              <span
+                className={`px-4 py-2 inline-flex text-xs leading-5 font-semibold rounded-full ${labelStyle}`}
+              >
+                {variant} variant
+              </span>
+            </div>
+            <p className="text-gray-600 mb-8 text-center sm:text-left" dangerouslySetInnerHTML={{ __html: text }} />
+          </div>
         </div>
       </div>
-    </div>
-    <UniformSlot name="content" />
-  </section>
-);
+    </section>
+  );
+};
+
+const ImageComponent = ({ image }: { image: string }) => {
+  return image ? (
+    <img
+      src={image}
+      alt="Why attend image"
+      width={400}
+      height={400}
+      loading="lazy"
+      className="object-cover rounded-lg shadow-md w-full sm:h-64 mx-auto"
+    />
+  ) : null;
+};
+
 registerUniformComponent({
   type: 'whyattend',
   component: WhyAttend,
